41f18014f56263ce173e644152bfc4bfee83c91b
ZiftK <alvaro.cab.lag@gmail.com>
2023-12-02 12:26:19 -0600

    del unuse msg class packages

--- src/pio/Msg.java
+++ src/pio/Msg.java
@@ -4,9 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.InputMismatchException;
 
 /**
  * Esta clase está diseñada para agilizar la entrada y salida
